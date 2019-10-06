7bc7c3c1f4d97176996d7731de32c3594be80428
Vigneshkumar R <43966345+vigneshkumar28@users.noreply.github.com>
2019-10-06 22:49:09 +0530

    python_data_structures
    python_data_structures

--- test.py
+++ test.py
@@ -1,14 +1,6 @@
-x = 'From marquard@uct.ac.za'
-find_at = x.find('@')
-print(find_at)
-find_dot = x.find('.')
-print(find_dot)
+stuff = dict()
+stuff['money'] = 1
+stuff['apples'] = 0
 
-uct_cut = x[find_at + 1: find_dot]
-print(uct_cut)
-
-print(len('banana')*7)
-
-data = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
-pos = data.find('.')
-print(data[pos:pos+3])
+stuff['google'] = 2
+print stuff
